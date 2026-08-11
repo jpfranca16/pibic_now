7fbc777a5e9c99fa312a4c73b6a1a9720ec2b445
João Pedro <joaopedro.fr16@gmail.com>
2026-08-11 19:55:12 -0300

    update

--- codigo/pagina_inicial.py
+++ codigo/pagina_inicial.py
@@ -17,14 +17,7 @@ from plotly.subplots import make_subplots
 # usando css para boniteza:
 st.markdown(
     """
-    <style>
-    /* Papel de parede de fundo */
-    .stApp {
-        background-image: url("https://images.unsplash.com/photo-1743046813915-94cf6d5e6942?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
-        background-attachment: fixed;
-        background-size: cover;
-    }
-
+    
     /* Fundo da Sidebar em Vidro Fosco */
     [data-testid="stSidebar"] {
         background-color: rgba(255, 255, 255, 0.70) !important;
